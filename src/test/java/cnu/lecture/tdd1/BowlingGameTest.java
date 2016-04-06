0d5521cd08b5faa747eb82acf264d0f30943dc79
seojungwon <garden715@gmail.com>
2016-04-06 16:17:22 +0900

    second

--- src/test/java/cnu/lecture/tdd1/BowlingGameTest.java
+++ src/test/java/cnu/lecture/tdd1/BowlingGameTest.java
@@ -1,13 +1,69 @@
 package cnu.lecture.tdd1;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 /**
  * Created by tchi on 2016. 4. 4..
  */
 public class BowlingGameTest {
-    @Test
-    public void testGutterGame() {
-        Game g = new Game();
-    }
+	private Game g;
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
+	}
+
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
+
+	@Test
+	public void testGutterGame() {
+		int n = 20;
+		int pins = 0;
+		rollMany(n, pins);
+		assertEquals(0, g.score());
+	}
+
+	@Test
+	public void testAllOnes() {
+		int n = 20;
+		int pins = 1;
+		rollMany(n, pins);
+		assertEquals(20, g.score());
+	}
+
+	@Test
+	public void testOneSpare() {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
+	@Test
+	public void testOneStrike() {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
+	}
+
+	@Test
+	public void testPerfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, g.score());
+	}
+
+	private void rollStrike() {
+		g.roll(10);
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
 }
